18ff0cab1506a675554305d7abde148da424f4c3
jwhisnant97 <jwhisnant97@gmail.com>
2026-03-15 17:15:28 -0500

    added get_file_content.py
    added config.py
    
    created lorem.txt > 25000 bytes Lorem Ipsum
    
    created test_get_file_content.py
    
    confirmed get_file_content working

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -9,7 +9,7 @@ def get_file_content(working_directory, file_path):
 
         target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
 
-        if os.path.commonpath(working_dir_abs, target_file) == working_dir_abs:
+        if os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs:
             pass
         else:
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
@@ -17,10 +17,10 @@ def get_file_content(working_directory, file_path):
         if not os.path.isfile(target_file):
             return f'Error: File not found or is not a regular file: "{file_path}"'
 
-        with open(file_path, "r") as f:
+        with open(target_file, "r") as f:
             file_content_string = f.read(MAX_CHARS)
             if f.read(1):
-                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
 
             return file_content_string
 
